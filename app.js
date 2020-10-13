3a963a0e29c9cba3705947c37c611366c695b183
= <garvitgalgat@gmail.com>
2020-10-13 22:55:50 +0530

    Procfile added and PORT changed

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get("*", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started on port " + process.env.PORT || 3000);
 });
