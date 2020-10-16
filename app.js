efb347ed6e3e98d9e4ed8fc8a3a9632a19aca4c7
= <garvitgalgat@gmail.com>
2020-10-16 18:54:03 +0530

    progess bar works on all pages and json button added

--- app.js
+++ app.js
@@ -16,13 +16,15 @@ app.get("/results", function (req, res) {
     var search = req.query.search;
     console.log("search is " + search);
     var url = `http://www.omdbapi.com/?apikey=thewdb&s=${search}`;
-    console.log(url);
+    console.log(req.query);
     var data;
     axios.get(url)
         .then(function (response){
             var results = response.data.Response;
             if(results.toLowerCase() == "true"){
                 data = response.data.Search;
+                if(req.query.json == 'checked')
+                    console.log(data);
                 res.render("results", {data: data, search: search});
 
             }
@@ -36,9 +38,9 @@ app.get("/results", function (req, res) {
         .then(function(){
             
         });
-        
-    
 });
+
+
 
 app.get("*", function(req, res){
     res.redirect("/");
--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@ function scrollFun() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  console.log(scrolled);
   document.getElementsByClassName("progress-bar")[0].style.width = scrolled + "%";
+
+  
 }
